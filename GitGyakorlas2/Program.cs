25c7ac74814426d480851e0ad2ceab787bdd64e6
Orlando-R <roglorand@gmail.com>
2021-04-12 14:28:43 +0200

    üres

---
+++ GitGyakorlas2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitGyakorlas2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Git próba push 2"); 
+            }
+            Console.ReadLine();
+        }
+    }
+}
